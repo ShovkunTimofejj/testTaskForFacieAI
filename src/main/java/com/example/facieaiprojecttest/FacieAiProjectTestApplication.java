29bb7de20cf0f015885d95f5b2d7ccebe169c7fb
shovk <shovkyntimofej@gmail.com>
2025-02-23 15:21:12 +0200

    init

---
+++ src/main/java/com/example/facieaiprojecttest/FacieAiProjectTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.facieaiprojecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacieAiProjectTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacieAiProjectTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/facieaiprojecttest/FacieAiProjectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facieaiprojecttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacieAiProjectTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
